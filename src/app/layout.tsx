4129b6cde621340d8a09eb1f040343e4fab8123a
aidoha.ibrv <aidyn.i@gb.solutions>
2023-10-05 14:47:49 +0600

    feat: map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
           {children}
         </Providers>
       </body>
+      {/* <script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
+      <script src="bower_components/datamaps/dist/datamaps.world.min.js"></script>
+      <div id="container" style="position: relative; width: 500px; height: 300px;"></div>
+      <script>
+          var map = new Datamap({element: document.getElementById('container')});
+      </script> */}
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,17 @@ import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useSearchParams } from "next/navigation";
 import MovieList from "@/components/MovieList";
 import { Movie } from "@/shared/types";
+import Map from "@/components/Map";
 
 export default function Home() {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre") || "popular";
   const { isLoading, isFetching, data, error } = useGetMoviesQuery({ genre });
 
-  console.log("data", data);
-  console.log("isLoading", isLoading);
-  console.log("isFetching", isFetching);
-  console.log("error", error);
-
   return (
     <div>
-      {data?.results && <MovieList list={data?.results!} />}
+      {/* {data?.results && <MovieList list={data?.results!} />} */}
+      <Map />
     </div>
   );
 }
